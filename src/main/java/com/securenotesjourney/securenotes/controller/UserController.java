ce316dac90665145571bff425f1900e35d8771db
Bruma Daniel <bruma.daniel@yahoo.com>
2026-08-13 20:21:32 +0300

    feat(user): add user entity, repository and REST endpoints
    - User entity mapped to users_table
    - UserRepository for data access via Spring Data JPA
    - POST /users/register and GET /users/{id}

---
+++ src/main/java/com/securenotesjourney/securenotes/controller/UserController.java
@@ -0,0 +1,26 @@
+package com.securenotesjourney.securenotes.controller;
+
+import com.securenotesjourney.securenotes.model.User;
+import com.securenotesjourney.securenotes.repository.UserRepository;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserRepository userRepository;
+
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @PostMapping("/register")
+    public User registerUser(@RequestBody User user) {
+        return userRepository.save(user);
+    }
+
+    @GetMapping("/{id}")
+    public User getUser(@PathVariable Long id) {
+        return userRepository.findById(id).orElse(null);
+    }
+}
